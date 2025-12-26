b65b0039a306915375658ed178b6b7aca747df4b
devman26173 <246410666+devman26173@users.noreply.github.com>
2025-12-26 21:11:21 +0900

    [251226] update Demo text

--- project-frontend/src/Demo.jsx
+++ project-frontend/src/Demo.jsx
@@ -1,7 +1,7 @@
 export default function Demo(){
     return (
         <div>
-            <h1 className="text-center">This is Text</h1>
+            <h1 className="text-center">This is Text...</h1>
         </div>
     )
 }
