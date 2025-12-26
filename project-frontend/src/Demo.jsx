5404ed441a25f3c876f32ad6ad96c1307febe6ed
devman26173 <246410666+devman26173@users.noreply.github.com>
2025-12-26 13:00:42 +0900

    [251226] Style init : boostrap

--- project-frontend/src/Demo.jsx
+++ project-frontend/src/Demo.jsx
@@ -1,7 +1,7 @@
 export default function Demo(){
     return (
         <div>
-            <h1>여기에 내용을 만들어주세요</h1>
+            <h1 className="text-center">ここに内容を追加してください</h1>
         </div>
     )
 }
