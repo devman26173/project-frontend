04bdb834ee7d71cb5242797edff22977f62850c6
devhyunju <246410195+devhyunju@users.noreply.github.com>
2025-12-30 13:17:52 +0900

    commit

--- project-frontend/src/Demo.jsx
+++ project-frontend/src/Demo.jsx
@@ -2,7 +2,7 @@ export default function Demo(){
     return (
         <div>
             <h1 className="text-center">ここに内容を入力てください</h1>
-            <h1>dfsdsfd</h1>
+            <h1 style="text-align: center; color: blue;"></div>맛집 게시판</h1>
         </div>
     )
 }
