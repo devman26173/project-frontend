ac57956bc89d1e72a09e11e51b1edfc56d56ec53
goodsujin <246410047+goodsujin@users.noreply.github.com.>
2025-12-30 13:18:54 +0900

    commit

--- project-frontend/src/Demo.jsx
+++ project-frontend/src/Demo.jsx
@@ -2,6 +2,9 @@ export default function Demo(){
     return (
         <div>
             <h1 className="text-center">内容を入力してください</h1>
+            <h1 style={{ textAlign: 'center', color: 'blue' }}>
+                ありがとうございます。
+            </h1>
         </div>
     )
 }
