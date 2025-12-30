aa58288609735abab8f3aab9133d358ec8382c33
devman26173 <246410666+devman26173@users.noreply.github.com>
2025-12-30 15:30:29 +0900

    [251230] merged commits bugs resolve

--- project-frontend/src/Demo.jsx
+++ project-frontend/src/Demo.jsx
@@ -3,7 +3,7 @@ export default function Demo(){
         <div>
             {/* hyunju */} 
             <h1 className="text-center">ここに内容を入力てください</h1>
-            <h1 style="text-align: center; color: blue;"></div>맛집 게시판</h1>
+            <h1 style={{ textAlign: 'center', color: 'blue' }}>見出し</h1>
             {/* sujin */} 
             <h1 className="text-center">内容を入力してください</h1>
             <h1 style={{ textAlign: 'center', color: 'blue' }}>
