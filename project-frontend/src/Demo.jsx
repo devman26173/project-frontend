2040dc217ef6aeadf1ce3cd1a2ef7fb37c8e45f0
devhyunju <246410195+devhyunju@users.noreply.github.com>
2025-12-30 12:40:29 +0900

    commit

--- project-frontend/src/Demo.jsx
+++ project-frontend/src/Demo.jsx
@@ -1,7 +1,8 @@
 export default function Demo(){
     return (
         <div>
-            <h1 className="text-center">ここに内容を入力ださい</h1>
+            <h1 className="text-center">ここに内容を入力てください</h1>
+            <h1>dfsdsfd</h1>
         </div>
     )
 }
