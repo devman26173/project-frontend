8b53b5c2bbe6c7b21be0803350792a7a518baef4
java0731kk <246409921+java0731kk : @users.noreply.github.com>
2025-12-30 13:20:34 +0900

    commit

--- project-frontend/src/Demo.jsx
+++ project-frontend/src/Demo.jsx
@@ -2,6 +2,7 @@ export default function Demo(){
     return (
         <div>
             <h1 className="text-center">ここに内容を入力してください</h1>
+            <h1 className="text-center text-green-500">楽しいランチタイム！</h1>
         </div>
     )
 }
