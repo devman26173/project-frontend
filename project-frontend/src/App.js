63e0689b9995eeaf0d09bcb954bcaf9cee896a2f
goodsujin <246410047+goodsujin@users.noreply.github.com.>
2026-01-05 17:43:20 +0900

    Update gitignore rules for untracked folders

--- project-frontend/src/App.js
+++ project-frontend/src/App.js
@@ -1,11 +1,11 @@
-import Demo from './Demo';
+
 import SignupForm from './signupfrom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Demo />
+       
         <SignupForm />
       </header>
     </div>
