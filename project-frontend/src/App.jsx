6956be01deff67cc5dca6c50d2a1af3dca34cb48
devman26173 <246410666+devman26173@users.noreply.github.com>
2026-01-05 13:03:56 +0900

    [260105] layout setting : responsive container

---
+++ project-frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import Demo from './Demo';
+
+function App() {
+  return (
+    <div className="App container vh-100 d-flex justify-content-center">
+      <Demo />
+    </div>
+  );
+}
+
+export default App;
