7e228e499cffa48554a09e89f50728b7dfc9693e
devman26173 <246410666+devman26173@users.noreply.github.com>
2026-01-06 13:03:42 +0900

    [260106] これまでの変更

--- project-frontend/src/App.jsx
+++ project-frontend/src/App.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Post from './components/Post';
-import Board from './Board';
 import SignupForm from './SignupForm';
 import Profile from './Profile';
 import { mockPost } from './mockPost';
+import RestaurantBoard from './RestaruantBoard';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="container vh-100 d-flex justify-content-center">
         <Routes>
-            <Route path="/board" element={<Board/>} />
+            <Route path="/board" element={<RestaurantBoard/>} />
             <Route path="/sign-up" element={<SignupForm/>} />
             <Route path="/profile" element={<Profile/>} />
             <Route path="/post" element={
