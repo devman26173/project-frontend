36bb8138c1aebd382faec2080b65dbd7b60df3a9
devman26173 <246410666+devman26173@users.noreply.github.com>
2026-01-07 14:58:43 +0900

    [260107] update router

--- project-frontend/src/Home.jsx
+++ project-frontend/src/Home.jsx
@@ -1,8 +1,28 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
-    return (
-        <div className="d-flex flex-column justify-content-end align-items-end p-2">
-            <button type="button" className="btn btn-dark mb-2 w-100">Login</button>
-            <button type="button" className="btn btn-dark w-100">Sign Up</button>
-        </div>
-    );
+  return (
+    <div className="container position-relative vh-100">
+      {/* 버튼 그룹 */}
+      <div className="position-absolute bottom-0 end-0 p-3" style={{ minWidth: "150px" }}>
+        <Link to="/login" className="d-block mb-2">
+          <button
+            type="button"
+            className="btn btn-dark w-100"
+          >
+            Login
+          </button>
+        </Link>
+
+        <Link to="/signup" className="d-block">
+          <button
+            type="button"
+            className="btn btn-dark w-100"
+          >
+            Sign Up
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
