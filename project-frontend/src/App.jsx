63e418395f4ac7dba00882af85661ce6b5317e67
devman26173 <246410666+devman26173@users.noreply.github.com>
2026-01-08 10:39:04 +0900

    [260108] remove app.js

---
+++ project-frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Post from './Post';
+import SignupForm from './SignupForm';
+import Profile from './Profile';
+import Login from './Login';
+import { mockPost } from './mockPost';
+import FoodBoard from './FoodBoard';
+import Home from './Home';
+import ProfileEdit from './ProfileEdit';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="container vh-100 d-flex justify-content-center">
+        <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/board" element={<FoodBoard/>} />
+            <Route path="/signup" element={<SignupForm/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/profile" element={<Profile/>} />
+            <Route path="/profile-edit" element={<ProfileEdit/>} />
+            <Route path="/post" element={
+              <div style={{ margin: "0 auto", padding: "20px" }}>
+                <Post post={mockPost} />
+              </div>} />
+            </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
